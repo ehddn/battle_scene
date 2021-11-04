30cf336b484ea438cd3491a2a44b81a27026fe51
고동우 <ehddn2202@naver.com>
2021-11-04 21:58:23 +0900

    플레이테스트

--- Battle_scene/Assets/Scenes/Scripts/Player_character.cs
+++ Battle_scene/Assets/Scenes/Scripts/Player_character.cs
@@ -36,6 +36,7 @@ public class Player_character : MonoBehaviour
     {
         rigid = GetComponent<Rigidbody2D>();
         enemy = GameObject.FindWithTag("Enemy").GetComponent<Enemy_character>();
+        isAttack = true;
     }
 
 
